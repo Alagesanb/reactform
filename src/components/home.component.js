a6126ec96e58a575be2614455112935a547c9012
alagesanb <alagesbalan@gmail.com>
2020-12-21 19:11:47 +0530

    first commit

---
+++ src/components/home.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Home.css"
+export default class Home extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Welcome</h3>
+            </form>
+        );
+    }
+}
